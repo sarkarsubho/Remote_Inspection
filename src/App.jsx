491e2b57ea47d4201e95de2529292723829819b0
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-04-21 01:32:25 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
 
   return (
     <>
-      <Camera />
+      {/* <Camera /> */}
       {/* <CameraApp/> */}
-      {/* <CameraComponent/> */}
+      <CameraComponent/>
     </>
   );
 }
