bd7aa5233e9d7a4cda497aad676c1b2c16bf0d46
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-04-21 01:27:16 +0530

    final draft sollution

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -3,80 +3,43 @@ import { v4 as uuidv4 } from "uuid";
 import { useGetCams } from "../hooks/hooks";
 
 const Camera = () => {
-  //   const [selectedCamera, setSelectedCamera] = useState("front");
-  //   const [aspectRatio, setAspectRatio] = useState("16:9");
-  //   const videoRef = useRef({});
-
-  //   const handleCameraToggle = () => {
-  //     setSelectedCamera(selectedCamera === "front" ? "back" : "front");
-  //   };
-
-  //   const handleAspectRatioChange = (newAspectRatio) => {
-  //     setAspectRatio(newAspectRatio);
-  //   };
-
-  //   const constraints = {
-  //     video: {
-  //       facingMode: selectedCamera,
-  //       aspectRatio: { ideal: eval(aspectRatio.replace(":", "/")) },
-  //     },
-  //   };
-
-  //   navigator.mediaDevices
-  //     .getUserMedia(constraints)
-  //     .then((stream) => {
-  //       videoRef.current.srcObject = stream;
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error accessing camera:", error);
-  //     });
-
-  //   return (
-  //     <div className="camera-app">
-  //       <div className="camera-controls">
-  //         <button onClick={handleCameraToggle}>Toggle Camera</button>
-  //         <div>
-  //           <label htmlFor="aspectRatio">Aspect Ratio:</label>
-  //           <select
-  //             id="aspectRatio"
-  //             value={aspectRatio}
-  //             onChange={(e) => handleAspectRatioChange(e.target.value)}
-  //           >
-  //             <option value="16:9">16:9</option>
-  //             <option value="4:3">4:3</option>
-  //             <option value="1:1">1:1</option>
-  //           </select>
-  //         </div>
-  //       </div>
-  //       <div className="camera-view">
-  //         <video ref={videoRef} autoPlay playsInline />
-  //       </div>
-  //     </div>
-  //   );
-  let vid = [];
-  // -------------------------------------------------------------------------------------
-  async function getCams() {
-    await navigator.mediaDevices.enumerateDevices().then((data) => {
-      let deviceIds = data
-        .filter((e) => e.kind === "videoinput")
-        .map((e) => vid.push(e.deviceId));
-
-      console.log("navigoter data", vid);
-    });
-  }
-
   const [openBackCamera, setOpenBackCamera] = useState(false);
+  const [mediaStream, setMediaStream] = useState(null);
   let [camIndex, setCamIndex] = useState(0);
-
-  //   let [cams] = useGetCams();
-  //   console.log("hookcams", cams);
-
   const [selectedCamera, setSelectedCamera] = useState("user"); // environment
   const [aspectRatio, setAspectRatio] = useState("16:9");
-  const videoRef = useRef({});
+  const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
   const [images, setImages] = useState([]);
+
+  const [getCamErr, setGetCamErr] = useState(false);
+
+  const startCamera = async (fm = "user") => {
+    setGetCamErr(false);
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+      setMediaStream(null);
+    }
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: fm },
+      });
+      setMediaStream(stream);
+      videoRef.current.srcObject = stream;
+    } catch (error) {
+      console.error("Error accessing camera:", error);
+      setGetCamErr(true);
+    }
+  };
+
+  const stopCamera = () => {
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+      setMediaStream(null);
+    }
+    startCamera({ exact: "environment" });
+  };
 
   const handleCameraToggle = () => {
     setSelectedCamera(selectedCamera === "user" ? "environment" : "user");
@@ -113,8 +76,8 @@ const Camera = () => {
   };
 
   useEffect(() => {
-    getCams();
-    console.log("camIndex", vid[camIndex]);
+    // getCams();
+    // console.log("camIndex", vid[camIndex]);
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then((stream) => {
@@ -124,85 +87,24 @@ const Camera = () => {
         console.error("Error accessing camera:", error);
       });
     // console.log(constraints);
-  }, [selectedCamera, camIndex]);
+  }, [selectedCamera, camIndex, aspectRatio]);
 
   console.log(constraints);
-  // ----------------------
-
-  // const [selectedCamera, setSelectedCamera] = useState("user"); // 'user' for front camera, 'environment' for back camera
-  // const [aspectRatio, setAspectRatio] = useState("16:9");
-  // const videoRef = useRef({});
-  // const canvasRef = useRef(null);
-
-  // const handleCameraToggle = () => {
-  //   setSelectedCamera(selectedCamera === "user" ? "environment" : "user");
-  // };
-
-  // const handleAspectRatioChange = (newAspectRatio) => {
-  //   setAspectRatio(newAspectRatio);
-  // };
-
-  // const handleCaptureImage = () => {
-  //   const canvas = canvasRef.current;
-  //   const video = videoRef.current;
-  //   canvas.width = video.videoWidth;
-  //   canvas.height = video.videoHeight;
-  //   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
-  //   const imageDataUrl = canvas.toDataURL("image/jpeg");
-  //   // Here you can handle the captured image data, such as saving it or displaying it in a gallery
-  //   console.log("Captured image:", imageDataUrl);
-  // };
-
-  // const constraints = {
-  //   video: {
-  //     facingMode: selectedCamera,
-  //     aspectRatio: { ideal: eval(aspectRatio.replace(":", "/")) },
-  //   },
-  // };
-
-  // useEffect(() => {
-  //   if (selectedCamera === "user") {
-  //     navigator.mediaDevices
-  //       .getUserMedia({
-  //         video: {
-  //           facingMode: "user",
-  //           aspectRatio: { ideal: eval(aspectRatio.replace(":", "/")) },
-  //         },
-  //       })
-  //       .then((stream) => {
-  //         videoRef.current.srcObject = stream;
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error accessing camera:", error);
-  //       });
-  //   } else {
-  //     console.log("getting environment variable");
-  //     navigator.mediaDevices
-  //       .getUserMedia({
-  //         video: {
-  //           facingMode: "environment",
-  //           aspectRatio: { ideal: eval(aspectRatio.replace(":", "/")) },
-  //         },
-  //       })
-  //       .then((stream) => {
-  //         videoRef.current.srcObject = stream;
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error accessing camera:", error);
-  //       });
-  //   }
-  // }, [selectedCamera]);
+  // // ----------------------
 
   return (
-    <div className="camera-app" style={{ width: "100vw" }}>
-      <div>
-        {/* {vid.map((e) => (
-          <p>{e}</p>
-        ))}{" "}
-        -------- index{camIndex} */}
-      </div>
+    <div className="camera-app">
+      {getCamErr ? (
+        <h3>Camera not Found !</h3>
+      ) : (
+        <div className="camera-view">
+          <video ref={videoRef} autoPlay playsInline />
+          <canvas ref={canvasRef} style={{ display: "none" }} />
+        </div>
+      )}
+
       <div className="camera-controls">
-        <button onClick={handleCameraToggle}>Toggle Camera</button>
+        {/* <button onClick={handleCameraToggle}>Toggle Camera</button> */}
         <div>
           <label htmlFor="aspectRatio">Aspect Ratio:</label>
           <select
@@ -217,10 +119,12 @@ const Camera = () => {
         </div>
         <button onClick={handleCaptureImage}>Capture Image</button>
       </div>
-      <div className="camera-view">
-        <video ref={videoRef} autoPlay playsInline />
-        <canvas ref={canvasRef} style={{ display: "none" }} />
+
+      <div>
+        <button onClick={startCamera}>Open Front Camera</button>
+        <button onClick={stopCamera}>Open Back Camera</button>
       </div>
+
       <div>
         <h1>Captured Images</h1>
 
